73762e243b594c6903919e8d031e24b81acf2677
Arvindan Naidu <52846982+arvindan1308@users.noreply.github.com>
2023-07-02 23:56:09 +0530

    Update docker-image-push-to-ecr.sh

--- docker-image-push-to-ecr.sh
+++ docker-image-push-to-ecr.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "loging to aws"
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 588933971286.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin account.id.dkr.ecr.us-east-1.amazonaws.com
 
 echo "docker build command"
-docker build -t my-docker-repo .
+docker build -t ecr-repo-name .
 
 echo "adding tag"
-docker tag my-docker-repo:latest 588933971286.dkr.ecr.us-east-1.amazonaws.com/my-docker-repo:latest
+docker tag my-docker-repo:latest ecr-uri:latest
 
 echo "pushing image to ecr"
-docker push 588933971286.dkr.ecr.us-east-1.amazonaws.com/my-docker-repo:latest
+docker push ecr-uri:latest
